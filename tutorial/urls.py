d1ae89e6a4d9ab65f28807b6e5389d3e403e0745
Aryaa S <aryaaspotlist@gmail.com>
2020-08-07 18:59:28 -0400

    adding x.txt

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -15,3 +15,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ##    path('snippets/', include('snippets.urls')),
 ]
+
+#this is a new comment !!
